d3517b09809f5d80b7b822ca0cb90ac249d17072
Julieta Raminger <julieta.raminger@gmail.com>
2023-12-23 12:06:55 -0300

    Creando mi pre-entrega

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
-//CALCULAR NOTA FINAL DE ALUMNOS INGRESADOS.
+/* CALCULAR COSTO TOTAL DE LOS PRODUCTOS CON O SIN DESCUENTO.
+✅Aplicar el descuento dependiendo de la cantidad.
+✅Ingresar la cantidad de productos para comprar.
+✅Ingresar nombre del produscto.
+✅Ingresar precio del producto.
+✅Ingresar la cantidad de ese producto que elegi para comprar.
+✅Poner un alert del total y si aplica descuento o no.
+✅Mostrar el total de todas las compras.
+*/
 
+let costoTotal;
+let numProductos;
+let nombreProducto;
+let precioProducto;
+let cantidad;
+let costoConDescuento;
+let totalCompra = 0;
 
-function calcularNotaFinal(nombre, promedio, asistencia) {
-    var criterioPromedio = 70; // Promedio necesario para aprobar
-    var criterioAsistencia = 80; // Porcentaje de asistencia necesario para aprobar
+function calcularCostoTotal(precioProducto, cantidad) {
+  costoTotal = precioProducto * cantidad;
 
-    if (promedio >= criterioPromedio && asistencia >= criterioAsistencia) {
-        return nombre + " está Aprobado";
-    } else {
-        return nombre + " está Reprobado";
-    }
+  if (cantidad >= 10 && cantidad < 20) {
+    costoTotal *= 0.9; // 10% de descuento
+  } else if (cantidad >= 20 && cantidad < 50) {
+    costoTotal *= 0.8; // 20% de descuento
+  } else if (cantidad >= 50) {
+    costoTotal *= 0.7; // 30% de descuento
+  }
+
+  return costoTotal;
 }
 
-function calcularNotasFinales() {
-    var continuarAgregando = true;
+numProductos = parseInt(prompt('Ingrese la cantidad de productos a comprar:'));
 
-    while (continuarAgregando) {
-        var nombre = prompt("Ingrese el nombre del alumno:");
-        var promedio = parseFloat(prompt("Ingrese el promedio del alumno:"));
-        var asistencia = parseFloat(prompt("Ingrese el porcentaje de asistencia del alumno:"));
+for (let i = 1; i <= numProductos; i++) {
+  nombreProducto = prompt('Ingrese el nombre del producto');
 
-        if (!isNaN(promedio) && !isNaN(asistencia)) {
-            var notaFinal = calcularNotaFinal(nombre, promedio, asistencia);
-            console.log(notaFinal);
+  precioProducto = parseFloat(prompt('Ingrese el precio del producto'));
 
-            var respuesta = prompt("¿Desea agregar otro alumno? (Si/No)").toLowerCase();
-            continuarAgregando = respuesta === 'si';
-        } else {
-            alert("Por favor, ingrese promedio y asistencia válidos.");
-        }
+  if (isNaN(precioProducto) || precioProducto <= 0) {
+    alert('Por favor, ingrese un precio válido y mayor que cero.');
+  }
+
+  do {
+    cantidad = parseInt(prompt('Ingrese la cantidad a comprar'));
+
+    if (isNaN(cantidad) || cantidad <= 0) {
+      alert('Por favor, ingrese una cantidad válida y mayor que cero.');
     }
+  } while (isNaN(cantidad) || cantidad <= 0);
+
+  costoConDescuento = calcularCostoTotal(precioProducto, cantidad);
+
+  if (cantidad < 10) {
+    alert(
+      'Estos productos no aplican descuento. El total es: $' +
+        costoConDescuento.toFixed(2)
+    );
+  } else {
+    alert(
+      'El costo total con descuento es de: $' + costoConDescuento.toFixed(2)
+    );
+  }
+
+  totalCompra += costoConDescuento;
 }
 
-calcularNotasFinales();
+alert('El total de la compra es: $' + totalCompra.toFixed(2));
