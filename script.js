8f9beef82ecb510d4090b7a0d243d5feb4b2ea91
Julieta Raminger <julieta.raminger@gmail.com>
2023-12-24 17:37:44 -0300

    Primera pre-entrega

--- script.js
+++ script.js
@@ -8,63 +8,73 @@
 ✅Mostrar el total de todas las compras.
 */
 
-let costoTotal;
-let numProductos;
-let nombreProducto;
-let precioProducto;
-let cantidad;
-let costoConDescuento;
-let totalCompra = 0;
-
-function calcularCostoTotal(precioProducto, cantidad) {
-  costoTotal = precioProducto * cantidad;
-
-  if (cantidad >= 10 && cantidad < 20) {
-    costoTotal *= 0.9; // 10% de descuento
-  } else if (cantidad >= 20 && cantidad < 50) {
-    costoTotal *= 0.8; // 20% de descuento
-  } else if (cantidad >= 50) {
-    costoTotal *= 0.7; // 30% de descuento
-  }
-
-  return costoTotal;
-}
-
-numProductos = parseInt(prompt("Ingrese la cantidad de productos a comprar:"));
-
-for (let i = 1; i <= numProductos; i++) {
-  nombreProducto = prompt("Ingrese el nombre del producto");
-
-  do {
-    precioProducto = parseFloat(prompt("Ingrese el precio del producto"));
-
-    if (isNaN(precioProducto) || precioProducto <= 0) {
-      alert("Por favor, ingrese un precio válido y mayor que cero.");
+const calcularCostoTotal = (precio, cantidad) => {
+    let costoTotal = precio * cantidad;
+  
+    if (cantidad >= 10 && cantidad < 20) {
+      costoTotal *= 0.9; // 10% de descuento
+    } else if (cantidad >= 20 && cantidad < 50) {
+      costoTotal *= 0.8; // 20% de descuento
+    } else if (cantidad >= 50) {
+      costoTotal *= 0.7; // 30% de descuento
     }
-  } while (isNaN(precioProducto) || precioProducto <= 0);
-
-  do {
-    cantidad = parseInt(prompt("Ingrese la cantidad a comprar."));
-
-    if (isNaN(cantidad) || cantidad <= 0) {
-      alert("Por favor, ingrese una cantidad válida y mayor que cero.");
+  
+    return costoTotal;
+  };
+  
+  const solicitarNumero = (mensaje) => {
+    let numero;
+  
+    do {
+      numero = parseInt(prompt(mensaje));
+  
+      if (isNaN(numero) || numero <= 0) {
+        alert('Por favor, ingrese un número válido y mayor que cero.');
+      }
+    } while (isNaN(numero) || numero <= 0);
+  
+    return numero;
+  };
+  
+  const solicitarDecimal = (mensaje) => {
+    let decimal;
+  
+    do {
+      decimal = parseFloat(prompt(mensaje));
+  
+      if (isNaN(decimal) || decimal <= 0) {
+        alert('Por favor, ingrese un número decimal válido y mayor que cero.');
+      }
+    } while (isNaN(decimal) || decimal <= 0);
+  
+    return decimal;
+  };
+  
+  const agregarProducto = () => {
+    const nombreProducto = prompt('Ingrese el nombre del producto');
+    const precioProducto = solicitarDecimal('Ingrese el precio del producto');
+    const cantidad = solicitarNumero('Ingrese la cantidad a comprar.');
+  
+    const costoConDescuento = calcularCostoTotal(precioProducto, cantidad);
+  
+    if (cantidad < 10) {
+      alert('Estos productos no aplican descuento. El total es: $' + costoConDescuento);
+    } else {
+      alert('El costo total con descuento es de: $' + costoConDescuento);
     }
-  } while (isNaN(cantidad) || cantidad <= 0);
-
-  costoConDescuento = calcularCostoTotal(precioProducto, cantidad);
-
-  if (cantidad < 10) {
-    alert(
-      "Estos productos no aplican descuento. El total es: $" +
-        costoConDescuento.toFixed(2)
-    );
-  } else {
-    alert(
-      "El costo total con descuento es de: $" + costoConDescuento.toFixed(2)
-    );
-  }
-
-  totalCompra += costoConDescuento;
-}
-
-alert("El total de la compra es: $" + totalCompra.toFixed(2));
+  
+    return costoConDescuento;
+  };
+  
+  const realizarCompra = () => {
+    const numProductos = solicitarNumero('Ingrese la cantidad de productos a comprar:');
+    let totalCompra = 0;
+  
+    for (let i = 1; i <= numProductos; i++) {
+      totalCompra += agregarProducto();
+    }
+  
+    alert('El total de la compra es: $' + totalCompra);
+  };
+  
+  realizarCompra();
